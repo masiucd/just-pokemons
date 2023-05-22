f84109c07d5cb58a871a4c216839e16f22c2a4a9
marcell ciszek druzynski <ciszekmarcell@gmail.com>
2023-05-22 16:17:25 +0200

    Remove FAQ page

--- src/app/(marketing)/faq/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-import {PageWrapper} from "@/components/page_wrapper";
-
-const FaqPage = () => {
-  return <PageWrapper>FaqPage</PageWrapper>;
-};
-
-export default FaqPage;
--- src/app/(marketing)/layout.tsx
+++ src/app/(marketing)/layout.tsx
@@ -13,47 +13,6 @@ function Icon() {
   );
 }
 
-// function SearchForm() {
-//   async function handleSubmit(formData: FormData) {
-//     "use server";
-//     // Gets the search value from the form data
-//     const search = formData.get("search");
-//     if (search !== null && typeof search === "string") {
-//       const url = PokemonAPI.pokemonByName(search.toLowerCase());
-//       fetch(url)
-//         .then((res) => res.json())
-//         .then((data) => {
-//           console.log(data.name);
-//           revalidatePath(`/pokemon/${data.name}`);
-//           // redirect(`/pokemon/${data.name}`);
-//         })
-//         .catch((err) => {
-//           console.error(err);
-//           revalidatePath(`/`);
-//         });
-//     }
-//   }
-
-//   return (
-//     <form
-//       action={handleSubmit}
-//       className="flex h-full flex-1 rounded border border-slate-950"
-//     >
-//       <input
-//         type="text"
-//         className="flex-1 rounded pl-2  outline-none focus:shadow-2xl"
-//         name="search"
-//       />
-//       <button
-//         className="flex basis-14 items-center justify-center border-l-2 border-slate-950 bg-white hover:bg-slate-50"
-//         type="submit"
-//       >
-//         <Icons.search size={20} />
-//       </button>
-//     </form>
-//   );
-// }
-
 function Content() {
   return (
     <div className="flex h-10 flex-1 items-center gap-4">
@@ -61,9 +20,6 @@ function Content() {
         <ul className="flex gap-5">
           <li>
             <Link href="/about">About</Link>
-          </li>
-          <li>
-            <Link href="/faq">Faq</Link>
           </li>
           <li>
             <Link href="/pokemon">Pokemon</Link>
